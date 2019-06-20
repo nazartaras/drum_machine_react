f6d5929035184cba74e8cc9cd14183eae263972f
Taras <tarasnazar93@gmail.com>
2019-06-20 20:40:31 +0300

    Updates

---
+++ precache-manifest.c84031dc8978c0539526c920ca75a6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82b353f3ff8fa7bbec7dd709f39851",
+    "url": "/drum_machine_react/index.html"
+  },
+  {
+    "revision": "2eaf2c07fe2c0b4aa0ab",
+    "url": "/drum_machine_react/static/css/main.98523223.chunk.css"
+  },
+  {
+    "revision": "7705b820fe8929685368",
+    "url": "/drum_machine_react/static/js/2.5068b10f.chunk.js"
+  },
+  {
+    "revision": "2eaf2c07fe2c0b4aa0ab",
+    "url": "/drum_machine_react/static/js/main.312c7f50.chunk.js"
+  },
+  {
+    "revision": "baef12bf28070f1fd934",
+    "url": "/drum_machine_react/static/js/runtime~main.f3411cf1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum_machine_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
